cfad00610d06ea943a9ad30a20c33ae9e6046c2e
Sandra-SFR <sandrafantonirubio@gmail.com>
2023-10-16 22:54:49 +0200

    estilo formulario registro y login terminado, mapa de prueba en el dashboard con nav responsive

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -28,7 +28,7 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
             ->add('envio', CheckboxType::class)
-            ->add('plainPassword', PasswordType::class, [
+            ->add('password', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
